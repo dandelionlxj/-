71ae43b17c07f3580811919eb72ec1d549542de4
dandelionlxj <1611462783@qq.com>
2018-09-07 17:20:43 +0800

    o

--- wc.py
+++ wc.py
@@ -1,3 +1,10 @@
+import argparse
+parser=argparse.ArgumentParser(description='this is wc.exe')
+parser.add_argument('-c', dest='char' )
+parser.add_argument('-w', dest='word' )
+parser.add_argument('-l', dest='line' )
+args=parser.parse_args()
+
 file_name='D:/python/alien/button.py'
 def Line_count(file_name):#统计行数
 	lines=0
@@ -27,6 +34,9 @@ line_count=Line_count(file_name)
 word_count=Word_count(file_name)
 character_count=Character_count(file_name)
 
-print('行数：',line_count)
-print('单词数：',word_count)
-print('字母数：',character_count)
+if args.line:
+	print('行数：',line_count)
+if args.word:
+	print('单词数：',word_count)
+if args.char:
+	print('字母数：',character_count)
